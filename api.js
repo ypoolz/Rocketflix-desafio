424723d7df0b4a69670587b2b3a43006735b1c68
ypoolz <snt.biel15@gmail.com>
2022-07-27 15:07:25 -0300

    up

---
+++ api.js
@@ -0,0 +1,5 @@
+export const language = 'language=en-US';
+export const API_KEY = 'api_key=ee498eaecce99626208f2ac077e1dbc1';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY+'&'+language;
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500'
